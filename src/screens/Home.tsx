aba345243dd781a3f2bca22b6fb4f0235c8945a3
Carlos Diez <cdiezmoran@gmail.com>
2021-10-25 00:36:31 -0500

    Challenge specs

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,25 @@
+/**
+ * API URL: /api/order/all?restaurantID=600c6ede63226ae92efe9bff
+ * query: {
+ *    restaurantID: string;
+ *    first: number;
+ *    offset: number;
+ * }
+ * 
+ * 
+ * Challenge: Debes de hacer una llamada al endpoint proporcionado, cargar las ordenes y renderizarlas
+ * en una lista utilizando el componente OrderRow.
+ * Extra: Manejar pagination usando first y offset en el API.
+ * Extra: Hacer que el componente de Header se mueva junto con la lista.
+ */
+
+
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import OrderRow from '../components/OrderRow';
 import Header from '../components/HomeHeader';
-
-/**
- * API URL: /api/order/all?restaurantID=600c6ede63226ae92efe9bff
- * 
- * query: {
- *    restaurantID: string;
- *    first: number;
- *    offset: number;
- * }
- */
 
 const Home = () => (
   <SafeAreaView style={styles.container}>
