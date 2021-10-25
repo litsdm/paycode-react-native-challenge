3c4ead547d6063787a7c146769a0438cbfb5a54d
Carlos Diez <cdiezmoran@gmail.com>
2021-10-25 00:14:27 -0500

    API url spec

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,6 +5,16 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import OrderRow from '../components/OrderRow';
 import Header from '../components/HomeHeader';
 
+/**
+ * API URL: /api/order/all?restaurantID=600c6ede63226ae92efe9bff
+ * 
+ * query: {
+ *    restaurantID: string;
+ *    first: number;
+ *    offset: number;
+ * }
+ */
+
 const Home = () => (
   <SafeAreaView style={styles.container}>
     <Header />
