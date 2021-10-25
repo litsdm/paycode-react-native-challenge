3dbdf768accb25b15cc200a72495cbb04bd51583
Carlos Diez <cdiezmoran@gmail.com>
2021-10-25 00:03:26 -0500

    Remove temp

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,24 +5,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import OrderRow from '../components/OrderRow';
 import Header from '../components/HomeHeader';
 
-const tempRow = {
-  createdOn: '2021-04-26T16:23:48.393+00:00',
-  completedOn: '2021-04-26T16:23:48.393+00:00',
-  name: 'Orden #1',
-  table: {
-    name: 'Mesa 1'
-  }
-}
-
 const Home = () => (
   <SafeAreaView style={styles.container}>
     <Header />
-    <OrderRow 
-      name={tempRow.name} 
-      createdOn={tempRow.createdOn} 
-      completedOn={tempRow.completedOn} 
-      tableName={tempRow.table.name} 
-    />
   </SafeAreaView>
 );
 
